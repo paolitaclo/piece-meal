7688ae98ada5b2916dc5eded4838ba7f35ef5bc8
Paola Claros <paolita.claros@gmail.com>
2017-04-05 16:06:30 -0700

    add search bar, logo and create profile form

--- src/components/create_profile_button.js
+++ src/components/create_profile_button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 // import { Text } from 'react-bootstrap';
 
-const CreateProfile = () => {
+const CreateProfileButton = () => {
   return (
     <Navbar.Text pullRight>
         <a href="#">Create Profile</a>
@@ -10,4 +10,4 @@ const CreateProfile = () => {
   )
 };
 
-export default CreateProfile;
+export default CreateProfileButton;
--- src/components/navigation_bar.js
+++ src/components/navigation_bar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import SearchBar from './search_bar';
+// import BrowseFilter from './browse_filter';
 import BrandLogo from './brand_logo';
-import CreateProfile from './create_profile';
+import CreateProfileButton from './create_profile_button';
 
 const NavigationBar = () => {
   return (
     <Navbar>
       <BrandLogo />
-      <SearchBar/>
-      <CreateProfile />
+      <SearchBar />
+      <CreateProfileButton />
     </Navbar>
   )
 }
