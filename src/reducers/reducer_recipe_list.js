87e323ca79771c012ae79ce25c0fcf6616c9570e
Paola Claros <paolita.claros@gmail.com>
2017-04-11 23:04:21 -0700

    fixed https url for images and sources

--- src/reducers/reducer_recipe_list.js
+++ src/reducers/reducer_recipe_list.js
@@ -5,7 +5,7 @@ export default function() {
       name: 'Fried Chicken Sandwich with Jalapeno Broccoli Slaw and Spicy Mayo',
       recipeAuthor: 'New South Food Co',
       servingsNumber: '4',
-      recipeUrl: 'http://newsouthfoodcompany.com/fried-chicken-sandwich-with-jalapeno-broccoli-slaw-and-spicy-mayo/',
+      recipeUrl: 'https://newsouthfoodcompany.com/fried-chicken-sandwich-with-jalapeno-broccoli-slaw-and-spicy-mayo/',
       ingredients: ['chicken', 'bread', 'flour', 'eggs', 'broccoli slaw', 'jalapeño', 'red onion', 'mayonnaise', 'pickles', 'buttermilk'],
       instructions:
       [
@@ -20,12 +20,12 @@ export default function() {
           steps: ['Combine flour, pepper, and 1/2 teaspoons salt in a gallon size ziploc bag.', 'Pour buttermilk into bowl.', 'Clean any remaining fat from chicken.', 'Slice the breasts horizontally so that you get 2 pieces from 1 whole chicken breast.', 'Working with 1 piece at a time, dredge chicken in flour mixture, shaking off excess. With your clean hand then dip in buttermilk, allowing excess to drip back into bowl. With your dry hand dredge again in flour mixture, shaking off excess.', 'Do this to all chicken pieces and set aside on a plate while you prepare the oil.', 'Pour oil or shortening into a large heavy skillet to a depth of 1/2-inch.', 'Heat over medium heat until thermometer registers 350°.', 'Fry chicken until golden brown and cooked through, about 5 minutes per side. Transfer to a wire rack set inside a baking sheet; season with kosher salt.', 'You can keep the fried chicken pieces in the oven at 170 degrees f while you fry the remaining pieces', 'Spread cut sides of buns with butter. Heat another large skillet over medium heat. Working in batches, cook buns buttered side down until browned and crisp, about 1 minute.', 'Spread with spicy mayo. Build sandwiches with buns, mayo, chicken, slaw and pickles.']
         }
       ],
-      imageUrl: 'http://newsouthfoodcompany.com/wp-content/uploads/2013/05/Fried-Chicken-with-Jalapeno-Cole-Slaw-and-Spicy-Mayo.jpg'
+      imageUrl: 'https://newsouthfoodcompany.com/wp-content/uploads/2013/05/Fried-Chicken-with-Jalapeno-Cole-Slaw-and-Spicy-Mayo.jpg'
     },
     {
       id:2,
       recipeAuthor: 'Everyday Insanity',
-      recipeUrl: 'http://cgharris.blogspot.com/2012/10/almost-like-pioneer-womans-lasagna.html',
+      recipeUrl: 'https://cgharris.blogspot.com/2012/10/almost-like-pioneer-womans-lasagna.html',
       servingsNumber: '4-6',
       name: 'Pioneer Woman\'s Lasagna',
       ingredients: ['ground beef', 'sausage', 'garlic', 'salt', 'tomatoes', 'parsley', 'basil', ' cottage cheese', 'mozzarella cheese', 'eggs', 'flour'],
@@ -44,12 +44,12 @@ export default function() {
 
         }
       ],
-      imageUrl: 'http://1.bp.blogspot.com/-eN_c9yHmFTw/UFgdVXnTu2I/AAAAAAAACuU/RgJe8SMVrfQ/s640/001.JPG'
+      imageUrl: 'https://1.bp.blogspot.com/-eN_c9yHmFTw/UFgdVXnTu2I/AAAAAAAACuU/RgJe8SMVrfQ/s640/001.JPG'
     },
     {
       id:3,
       recipeAuthor: 'All Day I Dream About Food',
-      recipeUrl: 'http://alldayidreamaboutfood.com/2010/12/feta-stuffed-turkey-meatloaf-with-tzatziki-sauce-low-carb.html',
+      recipeUrl: 'https://alldayidreamaboutfood.com/2010/12/feta-stuffed-turkey-meatloaf-with-tzatziki-sauce-low-carb.html',
       servingsNumber: '6',
       name: 'Feta Stuffed Turkey Meatloof with Tzatziki Sauce',
       ingredients: ['yogurt', 'lemon juice', 'garlic', 'cucumber', 'dill', 'salt', 'onion', 'olive oil', 'pepper', 'spinach', 'ground turkey', 'almond meal', 'chicken stock', 'eggs', 'worcestershire sauce', 'feta cheese'],
@@ -70,14 +70,14 @@ export default function() {
             'Cut into slices and drizzle with tzatziki sauce, passing remaining sauce at the table.']
         }
       ],
-      imageUrl: 'http://3.bp.blogspot.com/_IxGTjg3JMUg/TQ98JyOWLyI/AAAAAAAAAZY/t75CH1T_69A/s1600/075.JPG'
+      imageUrl: 'https://3.bp.blogspot.com/_IxGTjg3JMUg/TQ98JyOWLyI/AAAAAAAAAZY/t75CH1T_69A/s1600/075.JPG'
     },
     {
       id: 4,
       name: 'Edamame Mash Sandwiches',
       recipeAuthor: 'hot for food',
       servingsNumber: '4',
-      recipeUrl: 'http://www.hotforfoodblog.com/recipes/2015/2/2/edamame-mash-sandwiches',
+      recipeUrl: 'https://www.hotforfoodblog.com/recipes/2015/2/2/edamame-mash-sandwiches',
       ingredients: ['1 C red cabbage, shredded', '1/2 C sauerkraut', '8 slices of thick cut bread of your choice', '4 tbsp vegan mayonnaise', '2 tomatoes, cut into slices', '1 1/2 C baby spinach', '1 1/2 C frozen shelled edamame', '2 garlic cloves', '1 tbsp lime juice', '2 tbsp fresh cilantro leaves', '1 tbsp fresh mint leaves', '1 tbsp fresh dill fronds', '1/4 tsp ground mustard', '1/4 tsp sea salt', '1/4 tsp ground pepper'],
       instructions: [
         {
@@ -101,7 +101,7 @@ export default function() {
       name: 'Scallops with spaghetti, crispy kale & pomegranates',
       recipeAuthor: 'hot for food',
       servingsNumber: '2',
-      recipeUrl: 'http://www.hotforfoodblog.com/recipes/2015/2/2/scallops-with-spaghetti-crispy-kale-pomegranates',
+      recipeUrl: 'https://www.hotforfoodblog.com/recipes/2015/2/2/scallops-with-spaghetti-crispy-kale-pomegranates',
       ingredients: ['6 king oyster mushrooms',
         '3 C hot water', '1/3 C rice wine vinegar (or half and half rice vinegar and white wine vinegar)', '1 pkg brown rice spaghetti (340g - we like Tinkyada brand brown rice spaghetti)', '3 tbsp olive oil (or use vegan butter for cooking the scallops)', '1/4 C finely chopped shallot', '1 garlic clove, minced', '1 C finely chopped leek', '1 C dry white wine', '1 C pasta water (reserved from cooking noodles)', '2 tsp sea salt', '1/2 tsp ground pepper', '1/2 tsp dried oregano', '1/2 tsp dried basil', '1/4 tsp dried chili peppers', '1 tbsp lemon juice', '3 C roughly chopped lacinato kale (stems removed)', '1/8 C pomegranate seeds'],
       instructions: [
@@ -127,7 +127,7 @@ export default function() {
       name: 'Ahi Tuna Poke Quinoa Bowl',
       servingsNumber: '2-4',
       recipeAuthor: 'i am a food blog',
-      recipeUrl: 'http://iamafoodblog.com/ahi-tuna-poke-quinoa-bowl/',
+      recipeUrl: 'https://iamafoodblog.com/ahi-tuna-poke-quinoa-bowl/',
       ingredients: ['2 tablespoons soy sauce',
         '1/2 tablespoon sweet soy sauce', '1 teaspoon toasted sesame oil', '1/2 teaspoon finely grated fresh ginger', '/2 teaspoon finely grated garlic', '1 lb. fresh ahi (sashimi grade) tuna, cut into bite-size cubes', '1/4 cup sliced green onions', '1/4 cup finely diced onion', 'smoked sea salt, to taste', 'toasted sesame seeds, to taste', '1 avocado, peeled, pitted, and sliced', 'short grain white rice, or quinoa, to serve', 'edamame, if desired', 'sprouts, if desired', 'crispy shallots, if desired'],
       instructions: [
@@ -138,7 +138,7 @@ export default function() {
           `When ready to serve, taste the poke and adjust the seasoning as needed with extra soy sauce, sesame oil, and smoked sea salt. Scoop out an appropriate amount of rice or quinoa into bowls. Add a generous amount of poke to the bowls and sprinkle on sesame seeds. Finish the bowls with avocado slices, edamame, sprouts, and crispy shallots. Enjoy!`]
         }
       ],
-      imageUrl: 'http://iamafoodblog.com/wp-content/uploads/2016/01/poke-5.jpg'
+      imageUrl: 'https://iamafoodblog.com/wp-content/uploads/2016/01/poke-5.jpg'
     }
     ,
     {
@@ -146,7 +146,7 @@ export default function() {
       name: 'Bangkok Curry Noodle Bowls with Crisp Baked Tofu',
       servingsNumber: '2-4',
       recipeAuthor: 'Forbidden Rice Blog',
-      recipeUrl: 'http://forbiddenriceblog.com/tag/curry-noodles/',
+      recipeUrl: 'https://forbiddenriceblog.com/tag/curry-noodles/',
       ingredients: [
         '15.5 ounces extra firm tofu',
         '1 tablespoon toasted sesame oil',
@@ -193,13 +193,13 @@ export default function() {
           steps: [`serve bowls topped with the purple cabbage and sesame seeds, along with a squeeze of lime and chopped basil leaves. Enjoy!`]
         }
       ],
-      imageUrl: `http://forbiddenriceblog.com/wp-content/uploads/2016/01/Forbidden-Rice-Blog-Bangkok-Curry-Noodles-with-Crisp-Tofu-11-of-11.jpg`
+      imageUrl: `https://forbiddenriceblog.com/wp-content/uploads/2016/01/Forbidden-Rice-Blog-Bangkok-Curry-Noodles-with-Crisp-Tofu-11-of-11.jpg`
     },
     {
       id: 8,
       name: 'Wheat Berry Shickpea Salad with Spinach, Apples and Feta/',
       recipeAuthor: 'Dinner with Julie',
-      recipeUrl: 'http://www.dinnerwithjulie.com/2017/02/10/blog-flog-wheat-berry-chickpea-salad-with-spinach-apples-and-feta/',
+      recipeUrl: 'https://www.dinnerwithjulie.com/2017/02/10/blog-flog-wheat-berry-chickpea-salad-with-spinach-apples-and-feta/',
       servingsNumber: ' 4-6',
       ingredients: [
         '1/2 cup wheat berries',
@@ -220,7 +220,7 @@ export default function() {
           'Put the wheat berries into a bowl with the spinach, chickpeas, celery, apple, parsley and feta. In a small bowl or jar, whisk or shake together the vinegar, oil, mustard and honey. Drizzle over the salad and toss to coat. Season with salt and pepper and sprinkle with almonds just before serving.']
         }
       ],
-      imageUrl: `http://www.dinnerwithjulie.com/wp-content/uploads/2017/01/Wheat-Berry-Chickpea-Salad-1.jpg`
+      imageUrl: `https://www.dinnerwithjulie.com/wp-content/uploads/2017/01/Wheat-Berry-Chickpea-Salad-1.jpg`
     }
 
   ]
